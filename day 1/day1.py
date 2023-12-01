aa4156840693eb5bfea224882c00538d4062496a
Logan Quick <loganq@uchicago.edu>
2023-11-30 23:59:25 -0600

    Refactored Day 1

--- day 1/day1.py
+++ day 1/day1.py
@@ -1,38 +1,24 @@
 # Input
 lines = open('day1.txt', 'r').readlines()
 
-# Part 1
-sum1 = 0
-for line in lines:
-    i = 0
-    while not line[i].isdigit():
-        i += 1
-    tens = int(line[i])*10
-    i = len(line) - 1
-    while not line[i].isdigit():
-        i -= 1
-    ones = int(line[i])
-    sum1 += tens + ones
+# Functions
+def getAnswer(lines, numChecker):
+    result = 0
+    for line in lines:
+        tens, ones = 0, 0
+        for i in range(len(line)):
+            hit, num = numChecker(line, i)
+            if hit:
+                tens += 0 if tens != 0 else num*10
+                ones = num
+        result += tens + ones
+    return result
 
-print("Answer for Part 1:")
-print(sum1)
-    
+def numChecker1(string, pos):
+    return (True, int(string[pos])) if string[pos].isdigit() else (False, 0)
 
-# Part 2
-
-nums = {
-    "one":   1,
-    "two":   2,
-    "three": 3,
-    "four":  4,
-    "five":  5,
-    "six":   6,
-    "seven": 7,
-    "eight": 8,
-    "nine":  9,
-}
-
-def CheckForNum(string, pos):
+nums = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine":  9}
+def numChecker2(string, pos):
     if string[pos].isdigit():
         return (True, int(string[pos]))
     for length in [3,4,5]:
@@ -40,25 +26,8 @@ def CheckForNum(string, pos):
             return (True, nums[string[pos:pos+length]])
     return (False, 0)
 
-sum2 = 0
-for line in lines:
-    i = 0
-    tens = 0
-    while True:
-        hit, num = CheckForNum(line, i)
-        if hit:
-            tens = num*10
-            break
-        i += 1
-    i = len(line) - 1
-    ones = 0
-    while True:
-        hit, num = CheckForNum(line, i)
-        if hit:
-            ones = num
-            break
-        i -= 1
-    sum2 += tens + ones
-    
+# Results
+print("Answer for Part 1:")
+print(getAnswer(lines, numChecker1))
 print("Answer for Part 2:")
-print(sum2)
+print(getAnswer(lines, numChecker2))
