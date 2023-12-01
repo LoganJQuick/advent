abbc757d35e0df670c9a3fc7359202b865f36c5b
Logan Quick <loganq@uchicago.edu>
2023-12-01 00:11:48 -0600

    More refined? Debatable

--- day 1/day1.py
+++ day 1/day1.py
@@ -7,24 +7,24 @@ def getAnswer(lines, numChecker):
     for line in lines:
         tens, ones = 0, 0
         for i in range(len(line)):
-            hit, num = numChecker(line, i)
-            if hit:
-                tens += 0 if tens != 0 else num*10
+            num = numChecker(line, i)
+            if num:
+                tens += (tens==0)*num*10
                 ones = num
         result += tens + ones
     return result
 
 def numChecker1(string, pos):
-    return (True, int(string[pos])) if string[pos].isdigit() else (False, 0)
+    return int(string[pos]) if string[pos].isdigit() else None
 
 nums = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine":  9}
 def numChecker2(string, pos):
     if string[pos].isdigit():
-        return (True, int(string[pos]))
+        return int(string[pos])
     for length in [3,4,5]:
         if string[pos:pos+length] in nums:
-            return (True, nums[string[pos:pos+length]])
-    return (False, 0)
+            return nums[string[pos:pos+length]]
+    return None
 
 # Results
 print("Answer for Part 1:")
