2a491c84eef80246f77b29d5fb6d5f4ff00e4478
Logan Quick <loganq@uchicago.edu>
2023-12-03 12:59:41 -0600

    code speed

--- has33.py
+++ has33.py
@@ -1,5 +1,6 @@
 import timeit
 import random
+import numpy
 
 def has33(number_list):
     return any(number_list[i] == 3 and number_list[i + 1] == 3 for i in range(len(number_list) - 1))
@@ -7,17 +8,16 @@ def has33(number_list):
 def has33_v2(number_list):
     return '33' in "".join(str(number_list))
 
-rand_lists = [[random.randint(0, 9) for _ in range(20)] for _ in range(10000)]
+rand_lists = [random.randint(0, 9) for _ in range(10000000)]
+# rand_lists = numpy.array(rand_lists)
 
 
 before = timeit.default_timer()
-for rand_list in rand_lists:
-    has33(rand_list)
+has33(rand_lists)
 after = timeit.default_timer()
-print(after - before) # 0.0299 Seconds 
+print(f"Logan's Method: {after - before}") # 2.930e-05 seconds
 
 before = timeit.default_timer()
-for rand_list in rand_lists:
-    has33_v2(rand_list)
+has33_v2(rand_lists)
 after = timeit.default_timer()
-print(after - before) # 0.0517 seconds
+print(f"Randy's Method: {after - before}" ) # 1.948 seconds
